efe7040f1f14ae88ec6f9f5f93f37fc91c535ce8
Thomas-Trehou <thomastrehou.pro@gmail.com>
2024-02-07 16:16:50 +0100

    feat(Auth_JWT):users.controller/signIn and signOff

---
+++ app/controllers/users.controller.js
@@ -0,0 +1,48 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import "../helpers/env.load.js";
+import usersDatamapper from "../datamappers/users.datamapper.js";
+
+const JWTSecret = process.env.JWT_SECRET;
+const JWTRefreshExpiration = process.env.JWT_REFRESH_EXPIRATION;
+
+export default {
+
+  //  fonction de connexion
+  signIn: async (request, response) => {
+    const { email, password } = request.body;
+
+    const user = await usersDatamapper.findUserByEmail(email);
+    //  vérifie que l'utilisateur existe
+    if (!user) {
+      return response.status(401);
+    }
+    //  vérifie que le password encrypté est correct avec la base de donnée
+    const validPassword = await bcrypt.compare(password, user.password);
+
+    if (!validPassword) {
+      return response.status(401);
+    }
+    //  donne un token a l'utilisateur après les vérification
+    const token = jwt.sign({ id: user.id }, JWTSecret, {
+      expiresIn: JWTRefreshExpiration,
+    });
+
+    return response.status(200).send({
+      slug: user.slug,
+      pseudo: user.pseudo,
+      email: user.email,
+      accessToken: token,
+    });
+  },
+
+  //  fonctin de déconnexion
+  signOff: async (request, response) => {
+    const accessToken = request.body;
+
+    accessToken.destroy();
+
+    return response.status(200);
+  },
+
+};
