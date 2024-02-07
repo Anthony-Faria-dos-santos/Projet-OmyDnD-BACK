80d3e2f380e453821303c7a5b357fe59e9805c25
Thomas-Trehou <thomastrehou.pro@gmail.com>
2024-02-07 18:51:50 +0100

    feat(Auth_JWT):users.router/signup(post) and validateFactory on signin/signup

--- app/routers/routers/users.router.js
+++ app/routers/routers/users.router.js
@@ -1,8 +1,20 @@
 import { Router } from "express";
 import usersController from "../../controllers/users.controller.js";
 import controllerWrapper from "../../helpers/controller.wrapper.js";
+import validateFactory from "../../middlewares/validation.middleware.js";
+import signinSchema from "../../schemas/signin.schema.js";
+import signupSchema from "../../schemas/signup.schema.js";
 
 const router = Router();
 
-router.route("/signIn")
-  .post(controllerWrapper(usersController.signIn));
+router.route("/signin")
+  .post(
+    validateFactory("body", signinSchema),
+    controllerWrapper(usersController.signIn),
+  );
+
+router.route("/signup")
+  .post(
+    validateFactory("body", signupSchema),
+    controllerWrapper(usersController.signUp),
+  );
