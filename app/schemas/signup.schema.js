2cc6075474e75c8a90b28ea2c69769c211b5e760
Thomas-Trehou <thomastrehou.pro@gmail.com>
2024-02-07 22:06:27 +0100

    feat(Auth_JWT):schemas/signup.schema and users.controller/singUp (edit slug and passwordConfirm)

--- app/schemas/signup.schema.js
+++ app/schemas/signup.schema.js
@@ -8,15 +8,11 @@ export default Joi.object({
     .min(3)
     .max(15)
     .required(),
-  slug: Joi.string()
-    .pattern(pattern)
-    .min(3)
-    .max(15)
-    .required(),
   email: Joi.string()
     .email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "fr"] } })
     .required(),
   password: Joi.string()
     .min(8)
     .required(),
+  passwordConfirm: Joi.ref("password"),
 });
