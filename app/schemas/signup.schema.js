1651f4e111af41e1899cdf408a08f1225e03df8e
Thomas-Trehou <thomastrehou.pro@gmail.com>
2024-02-07 18:52:48 +0100

    feat(Auth_JWT):signin.schema and signup.schema

---
+++ app/schemas/signup.schema.js
@@ -0,0 +1,22 @@
+import Joi from "joi";
+
+const pattern = /^[a-zA-Z0-9_]*$/;
+
+export default Joi.object({
+  pseudo: Joi.string()
+    .pattern(pattern)
+    .min(3)
+    .max(15)
+    .required(),
+  slug: Joi.string()
+    .pattern(pattern)
+    .min(3)
+    .max(15)
+    .required(),
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "fr"] } })
+    .required(),
+  password: Joi.string()
+    .min(8)
+    .required(),
+});
