a0e2332b733423d88f10adc9e76e07f99dc43b8d
Thomas-Trehou <thomastrehou.pro@gmail.com>
2024-02-07 22:22:47 +0100

    docs:add comments

--- app/datamappers/users.datamapper.js
+++ app/datamappers/users.datamapper.js
@@ -1,6 +1,6 @@
 /* eslint-disable quotes */
 import client from "../helpers/pg.client.js";
-
+// fonction pour rechercher un user par son email
 export async function findUserByEmail(email) {
   const query = {
     text: 'SELECT * FROM "users" WHERE email=$1',
@@ -9,7 +9,7 @@ export async function findUserByEmail(email) {
   const result = await client.query(query);
   return result.rows[0];
 }
-
+// fonction pour selectionner tout les users qui qui correspondent au pseudo,slug ou email
 export async function checkUsersInformations(pseudo, slug, email) {
   const query = {
     text: 'SELECT * FROM "users" WHERE pseudo=$1 OR slug=$2 OR email=$3',
@@ -18,7 +18,7 @@ export async function checkUsersInformations(pseudo, slug, email) {
   const result = await client.query(query);
   return result.rows;
 }
-
+// fonction pour ajouter un user à la db
 export async function createUser(pseudo, slug, email, encryptedPassword) {
   const query = {
     text: 'INSERT INTO "users" ("pseudo", "slug", "email", "password") VALUES ($1, $2, $3, $4) RETURNING pseudo, slug, email, created_at',
--- app/schemas/signin.schema.js
+++ app/schemas/signin.schema.js
@@ -1,5 +1,5 @@
 import Joi from "joi";
-
+// on vérifie les entrées utilisateurs du signIn avant les utiliser
 export default Joi.object({
   email: Joi.string()
     .email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "fr"] } })
