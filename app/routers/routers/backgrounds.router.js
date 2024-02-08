f1e5c213825d24529193e6a14a3ccdecf5d38841
MickaelCarlucci <frewmike17@gmail.com>
2024-02-08 20:23:06 +0100

    feat(charaCreator): add router for All Backgrounds

---
+++ app/routers/routers/backgrounds.router.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import backgroundsController from "../../controllers/backgrounds.controller.js";
+import controllerWrapper from "../../helpers/controller.wrapper.js";
+
+const router = Router();
+
+router.route("/characterCreator")
+  .get(
+    controllerWrapper(backgroundsController.sendBackgrounds),
+  );
+
+export default router;
