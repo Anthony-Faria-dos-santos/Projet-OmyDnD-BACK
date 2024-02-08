8982b32fe642a520620a70b3a99c8f7ffc0d426a
MickaelCarlucci <frewmike17@gmail.com>
2024-02-08 20:25:14 +0100

    fix(charaCreator): change wrong name function

--- app/controllers/classes.controller.js
+++ app/controllers/classes.controller.js
@@ -2,7 +2,7 @@ import "../helpers/env.load.js";
 import classesDatamapper from "../datamappers/classes.datamapper.js";
 
 export default {
-  sendRaces: async (_, response) => {
+  sendClasses: async (_, response) => {
     const classes = await classesDatamapper.getAllClasses();
     response.status(200).send(classes);
   },
