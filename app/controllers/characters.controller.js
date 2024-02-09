8abb01e40c259787daeb2f5230c509ed51eb401e
Thomas-Trehou <thomastrehou.pro@gmail.com>
2024-02-09 19:12:01 +0100

    Feat(UsersCharacters): characters/deleteOneByPk + deleteAllOfOneUser, users.controller/deleteUserAccount add all characters deletion, users.router/.delete character

--- app/controllers/characters.controller.js
+++ app/controllers/characters.controller.js
@@ -68,7 +68,28 @@ export default {
     }
 
     return response.status(200).send(updatedCharacter);
-  }
+  },
 
+  deleteOneByPk: async (request, response) => {
+    const userId = request.params.userId;
+    const characterId = request.params.characterId;
+
+    const user = await usersDatamapper.findUserById(userId);
+
+    if (!user) {
+      return response.status(404).json({ error: "Utilisateur introuvable" });
+    };
+
+    const userCharacter = await charactersDatamapper.findOneByUserId(userId, characterId);
+
+    if (!userCharacter) {
+      return response.status(404).json({ error: "Le personnage demandé est introuvable" });
+    }
+
+    const deletedCharacter = await charactersDatamapper.deleteOne(characterId);
+
+    return response.status(200).send(deletedCharacter);
+
+  }
 
 }
