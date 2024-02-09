79f37743a288317a63e0e099448fd2753d6fd4e7
Thomas-Trehou <thomastrehou.pro@gmail.com>
2024-02-09 18:34:51 +0100

    Feat(UsersCharacters): characters.controller/updateOne and characters.controller/updateOne

--- app/controllers/characters.controller.js
+++ app/controllers/characters.controller.js
@@ -36,6 +36,39 @@ export default {
     return response.status(200).send(userCharacter);
   },
 
+  updateOne: async (request, response) => {
+    const userId = request.params.userId;
+    const characterId = request.params.characterId;
+    const character = request.body;
+
+    const user = await usersDatamapper.findUserById(userId);
+
+    if (!user) {
+      return response.status(404).json({ error: "Utilisateur introuvable" });
+    };
+
+    const userCharacter = await charactersDatamapper.findOneByUserId(userId, characterId);
+
+    if (!userCharacter) {
+      return response.status(404).json({ error: "Le personnage demandé est introuvable" });
+    }
+
+    const fields = Object.keys(character);
+    const values = Object.keys(character);
+
+    const placeholders = values.map((_, index) => `$${index + 1}`);
+    const characterIdPlaceholder = (values.length) + 1;
+
+    values.push(characterId);
+
+    const updatedCharacter = await charactersDatamapper.updateOne(fields, values, placeholders, characterIdPlaceholder);
+
+    if (!updatedCharacter) {
+      return response.status(500).json({ error: "Une erreur est survenue lors de la mise à jour de votre personnage" });
+    }
+
+    return response.status(200).send(updatedCharacter);
+  }
 
 
 }
