fa7308ecf33e19880fe1eb9f9f77276c630e9de1
Thomas-Trehou <thomastrehou.pro@gmail.com>
2024-02-10 21:54:20 +0100

    feat(UsersCharacters):characters.datamappers/findOneByUserId -> add character's skills and notes

--- app/datamappers/characters.datamapper.js
+++ app/datamappers/characters.datamapper.js
@@ -2,7 +2,7 @@ import client from "../helpers/pg.client.js";
 // fonction pour rechercher un user par son email
 export async function findAllCharactersByUserId(userId) {
   const query = {
-    text: `SELECT * FROM "characters" WHERE user_id=${userId}`,
+    text: `SELECT "name", "level", "status" FROM "characters" WHERE user_id=${userId}`,
   };
   const result = await client.query(query);
   return result.rows;
@@ -10,7 +10,20 @@ export async function findAllCharactersByUserId(userId) {
 
 export async function findOneByUserId(userId, characterId) {
   const query = {
-    text: `SELECT * FROM "characters" WHERE user_id=${userId} AND id=${characterId}`,
+    text: `SELECT
+            "characters".*,
+            array_agg(DISTINCT "characters_has_skills"."skill_id") as "skills",
+            array_agg(DISTINCT "notes"."title") as "notes_titles",
+            array_agg(DISTINCT "notes"."content") as "notes_contents"
+          FROM "characters"
+          JOIN "characters_has_skills"
+            ON "characters"."id" = "characters_has_skills"."character_id"
+          JOIN "notes"
+            ON "characters"."id" = "notes"."character_id"
+          WHERE "characters"."id" = ${characterId}
+            AND "characters"."user_id"= ${userId}
+          GROUP BY
+          "characters"."id";`,
   };
   const result = await client.query(query);
   return result.rows[0];
@@ -18,7 +31,7 @@ export async function findOneByUserId(userId, characterId) {
 
 export async function updateOne(fields, values, placeholders, characterIdPlaceholder) {
   const query = {
-    text: `UPDATE "characters" SET (${fields}) = (${placeholders}) WHERE id=${characterIdPlaceholder} RETURNING *`,
+    text: `UPDATE "characters" SET (${fields}) = (${placeholders}) WHERE id=$${characterIdPlaceholder} RETURNING *`,
     values,
   };
   const result = await client.query(query);
@@ -27,7 +40,7 @@ export async function updateOne(fields, values, placeholders, characterIdPlaceho
 
 export async function deleteOne(characterId) {
   const query = {
-    text: `DELETE FROM "characters" WHERE id=${characterId} RETURNING name`
+    text: `DELETE FROM "characters" WHERE id=${characterId} RETURNING name`,
   };
   const result = await client.query(query);
   return result.rows[0];
