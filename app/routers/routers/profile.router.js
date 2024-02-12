2357f6d4c4bc99732e718982a12c622d7e862601
MickaelCarlucci <frewmike17@gmail.com>
2024-02-12 17:39:30 +0100

    fix(profile): fix error with function update

--- app/routers/routers/profile.router.js
+++ app/routers/routers/profile.router.js
@@ -2,29 +2,35 @@ import { Router } from "express";
 import controllerWrapper from "../../helpers/controller.wrapper.js";
 import * as profileController from "../../controllers/profile.controller.js";
 import validationMiddleware from "../../middlewares/validation.middleware.js";
-import profileSchema from "../../schemas/profile.schema.js";
+import * as profileSchema from "../../schemas/profile.schema.js";
 
 const router = Router();
 // route pour modifier le pseudo et le slug
-router.route("/:pseudo")
+
+router.route("/:userId/profile")
+  .get(
+    controllerWrapper(profileController.getUser),
+  );
+
+router.route("/:usersId/profile/pseudo")
   .patch(
-    // vérifie la conformité du pseudo avec JOI
-    validationMiddleware("body", profileSchema),
+  // vérifie la conformité du pseudo avec JOI
+    validationMiddleware("body", profileSchema.pseudoValidation),
     // gére le try and catch dans le controller
     controllerWrapper(profileController.usernameModification),
   );
 
 // route pour modifier le password
-router.route("/:id/password")
+router.route("/:userId/profile/password")
   .patch(
-    validationMiddleware("body", profileSchema),
+    validationMiddleware("body", profileSchema.passwordValidation),
     controllerWrapper(profileController.passwordModification),
   );
 
 // route pour modifier l'email
-router.route("/:id/email")
+router.route("/:userId/profile/email")
   .patch(
-    validationMiddleware("body", profileSchema),
+    validationMiddleware("body", profileSchema.emailValidation),
     controllerWrapper(profileController.emailModification),
   );
 export default router;
