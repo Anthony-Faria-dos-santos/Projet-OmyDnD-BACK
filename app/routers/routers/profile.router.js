88dea84c940d323165c7c5a18ff02bc46c0d29f1
MickaelCarlucci <frewmike17@gmail.com>
2024-02-12 13:50:52 +0100

    feat(profile): add updatePassword in user profile

--- app/routers/routers/profile.router.js
+++ app/routers/routers/profile.router.js
@@ -1,12 +1,21 @@
 import { Router } from "express";
 import controllerWrapper from "../../helpers/controller.wrapper.js";
 import * as profileController from "../../controllers/profile.controller.js";
+import validationMiddleware from "../../middlewares/validation.middleware.js";
+import profileSchema from "../../schemas/profile.schema.js";
 
 const router = Router();
 
 router.route("/:pseudo")
   .patch(
+    validationMiddleware("body", profileSchema),
     controllerWrapper(profileController.usernameModification),
+  );
+
+router.route("/:id/password")
+  .patch(
+    validationMiddleware("body", profileSchema),
+    controllerWrapper(profileController.passwordModification),
   );
 
 export default router;
