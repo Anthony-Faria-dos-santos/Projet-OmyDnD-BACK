e0524cf9802e79a70dfaba1df1da176a7848a24f
MickaelCarlucci <frewmike17@gmail.com>
2024-02-15 12:17:42 +0100

    feat(swagger): add swagger doc in router

--- app/index.app.js
+++ app/index.app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import router from "./routers/index.router.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
+import createDoc from "./helpers/swagger.doc.js";
 
 const app = express();
 
@@ -16,6 +17,8 @@ app.use(cors({
   origin: "*",
 }));
 //! a gerer plus tard pour le parametrer que pour le front * l'ouvre a tous le monde pour le moment
+
+createDoc(app);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
