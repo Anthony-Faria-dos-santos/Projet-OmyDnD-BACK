f8dba74fdd8882f2613dc555c3e1404540573b67
MickaelCarlucci <frewmike17@gmail.com>
2024-02-15 15:38:09 +0100

    feat(swagger): add doc for character creator routers

--- app/routers/api/character.creator.router.js
+++ app/routers/api/character.creator.router.js
@@ -10,22 +10,51 @@ import characterCreatorSchema from "../../schemas/character.creator.schema.js";
 const router = Router();
 
 router.route("/creator")
+/**
+ * POST /api/charactercreator/creator
+ * @summary Create a new character
+ * @tags Character Creator
+ * @param {object} request.body.required necessary for the character
+ * @return {object} 200 - success response - application/json
+ * @return {ApiJsonError} 500 - Internal Server Error - application/json
+ */
   .post(
     validateFactory("body", characterCreatorSchema),
     controllerWrapper(characterCreatorController.createCharacter),
   );
 
 router.route("/races")
+/**
+ * GET /api/charactercreator/races
+ * @summary Get all races
+ * @tags Character Creator
+ * @return {object} 200 - success response - application/json
+ * @return {ApiJsonError} 500 - Internal Server Error - application/json
+ */
   .get(
     controllerWrapper(characterCreatorController.sendRaces),
   );
 
 router.route("/backgrounds")
+/**
+ * GET /api/charactercreator/backgrounds
+ * @summary Get all backgrounds
+ * @tags Character Creator
+ * @return {object} 200 - success response - application/json
+ * @return {ApiJsonError} 500 - Internal Server Error - application/json
+ */
   .get(
     controllerWrapper(characterCreatorController.sendBackgrounds),
   );
 
 router.route("/classes")
+/**
+ * GET /api/charactercreator/classes
+ * @summary Get all classes
+ * @tags Character Creator
+ * @return {object} 200 - success response - application/json
+ * @return {ApiJsonError} 500 - Internal Server Error - application/json
+ */
   .get(
     controllerWrapper(characterCreatorController.sendClasses),
   );
