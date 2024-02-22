6ed42431ead514deff9372f7c3fbafaab37f9210
Thomas-Trehou <thomastrehou.pro@gmail.com>
2024-02-22 09:26:05 +0100

    refactor: Add database check middleware for users and characters

--- app/routers/api/users.router.js
+++ app/routers/api/users.router.js
@@ -16,74 +16,108 @@ import validateFactory from "../../middlewares/validation.middleware.js";
 import signinSchema from "../../schemas/signin.schema.js";
 import signupSchema from "../../schemas/signup.schema.js";
 import * as profileSchema from "../../schemas/profile.schema.js";
+import dbCheck from "../../middlewares/database.check.middleware.js";
 
 const router = Router();
 
 router
   .route("/signin")
-  .post(
+  .post([
     validateFactory("body", signinSchema),
     controllerWrapper(usersController.signIn),
-  );
+  ]);
 
 router
   .route("/signup")
-  .post(
+  .post([
     validateFactory("body", signupSchema),
     controllerWrapper(usersController.signUp),
-  );
+  ]);
 
 router
-  .route("/:id(\\d+)/delete")
-  .delete(controllerWrapper(usersController.deleteUserAccount));
+  .route("/:userId(\\d+)/delete")
+  .delete([
+    controllerWrapper(dbCheck),
+    controllerWrapper(usersController.deleteUserAccount),
+  ]);
 
 router
-  .route("/:id(\\d+)/characters")
-  .get(controllerWrapper(charactersController.getAll));
+  .route("/:userId(\\d+)/characters")
+  .get([
+    controllerWrapper(dbCheck),
+    controllerWrapper(charactersController.getAll),
+  ]);
 
 router
   .route("/:userId(\\d+)/characters/:characterId(\\d+)")
-  .get(controllerWrapper(charactersController.getByPk))
-  .patch(controllerWrapper(charactersController.updateOne))
-  .delete(controllerWrapper(charactersController.deleteOneByPk));
+  .get([
+    controllerWrapper(dbCheck),
+    controllerWrapper(charactersController.getByPk),
+  ])
+  .patch([
+    controllerWrapper(dbCheck),
+    controllerWrapper(charactersController.updateOne),
+  ])
+  .delete([
+    controllerWrapper(dbCheck),
+    controllerWrapper(charactersController.deleteOneByPk),
+  ]);
 
 router
   .route("/:userId(\\d+)/characters/:characterId(\\d+)/skills/:skillId(\\d+)")
-  .post(controllerWrapper(charactersController.postSkill))
-  .delete(controllerWrapper(charactersController.deleteSkill));
+  .post([
+    controllerWrapper(dbCheck),
+    controllerWrapper(charactersController.postSkill),
+  ])
+  .delete([
+    controllerWrapper(dbCheck),
+    controllerWrapper(charactersController.deleteSkill),
+  ]);
 
 router
   .route("/:userId(\\d+)/characters/:characterId(\\d+)/notes/")
-  .post(controllerWrapper(charactersController.postNote));
+  .post([
+    controllerWrapper(dbCheck),
+    controllerWrapper(charactersController.postNote),
+  ]);
 
 router
   .route("/:userId(\\d+)/characters/:characterId(\\d+)/notes/:noteId(\\d+)")
-  .patch(controllerWrapper(charactersController.updateNote))
-  .delete(controllerWrapper(charactersController.deleteNote));
+  .patch([
+    controllerWrapper(dbCheck),
+    controllerWrapper(charactersController.updateNote),
+  ])
+  .delete([
+    controllerWrapper(dbCheck),
+    controllerWrapper(charactersController.deleteNote),
+  ]);
 
-router.route("/:userId(\\d+)/profile").get(controllerWrapper(getUser));
+router.route("/:userId(\\d+)/profile")
+  .get([controllerWrapper(dbCheck), controllerWrapper(getUser)]);
 
-router.route("/:userId(\\d+)/profile/pseudo").patch(
-  // vérifie la conformité du pseudo avec JOI
-  validateFactory("body", profileSchema.pseudoValidation),
-  // gére le try and catch dans le controller
-  controllerWrapper(usernameModification),
-);
+router.route("/:userId(\\d+)/profile/pseudo")
+  .patch([
+    validateFactory("body", profileSchema.pseudoValidation),
+    controllerWrapper(dbCheck),
+    controllerWrapper(usernameModification),
+  ]);
 
 // route pour modifier le password
 router
   .route("/:userId(\\d+)/profile/password")
-  .patch(
+  .patch([
     validateFactory("body", profileSchema.passwordValidation),
+    controllerWrapper(dbCheck),
     controllerWrapper(passwordModification),
-  );
+  ]);
 
 // route pour modifier l'email
 router
   .route("/:userId(\\d+)/profile/email")
-  .patch(
+  .patch([
     validateFactory("body", profileSchema.emailValidation),
+    controllerWrapper(dbCheck),
     controllerWrapper(emailModification),
-  );
+  ]);
 
 export default router;
