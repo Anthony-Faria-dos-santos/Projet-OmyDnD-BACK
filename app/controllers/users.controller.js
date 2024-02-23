3b5a91fd870e12c109325f911a2782cfaab71271
Thomas-Trehou <thomastrehou.pro@gmail.com>
2024-02-23 11:17:02 +0100

    Fix: Delete user request.param

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -93,7 +93,7 @@ export default {
   },
 
   deleteUserAccount: async (request, response) => {
-    const { userId } = request.params.userId;
+    const { userId } = request.params;
     const { email } = request.body;
 
     const user = await usersDatamapper.findUserById(userId);
